34e3f17f038169a5320c02e93755f6f0617ee7e6
nguytlinh <nguytlinh@users.noreply.github.com>
2021-05-27 17:54:24 -0400

    test pixart

--- pixmap_art.cpp
+++ pixmap_art.cpp
@@ -7,46 +7,33 @@ using namespace agl;
 int main(int argc, char** argv)
 {
 	ppm_image image;
-	image.load("../images/feep.ppm");
-	image.save("feep-test-save.ppm"); // should match original
-	cout << "loaded feep: " << image.width() << " " << image.height() << endl;
+	image.load("../images/earth-ascii.ppm");
+	image.save("earth-test-save.ppm"); // should match original
+	cout << "loaded earth: " << image.width() << " " << image.height() << endl;
 
 	// invert
-	ppm_image resize = image.resize(200, 300);
-	resize.save("earth-200-300.ppm");
+	ppm_image invert = image.invert();
+	invert.save("earth-invert.ppm");
 
 	// lightest
-	ppm_image grayscale = image.grayscale();
-	grayscale.save("earth-grayscale.ppm");
+	ppm_image lightest = image.lightest(invert);
+	lightest.save("earth-lightest.ppm");
 
 	// darkest
-	ppm_image flip = image.flip_horizontal();
-	flip.save("earth-flip.ppm");
+	ppm_image darkest = image.darkest(invert);
+	darkest.save("earth-darkest.ppm");
 
 	// difference
-	ppm_image sub = image.subimage(200, 200, 100, 100);
-	sub.save("earth-subimage.ppm");
+	ppm_image diff = image.difference(invert);
+	diff.save("earth-diff.ppm");
 
 	// multiply
-	ppm_image gamma = image.gammaCorrect(0.6f);
-	gamma.save("earth-gamma-0.6.ppm");
-
-	gamma = image.gammaCorrect(2.2f);
-	gamma.save("earth-gamma-2.2.ppm");
+	ppm_image multi = image.multiply(invert);
+	multi.save("earth-multiply.ppm");
 
 	// swirl
-	ppm_image soup;
-	soup.load("../images/soup-ascii.ppm");
+	ppm_image swirl = image.swirl();
+	invert.save("earth-swirl.ppm");
 
-
-
-	int y = (int)(0.5f * (image.width() - soup.width()));
-	int x = (int)(0.5f * (image.height() - soup.height()));
-	ppm_image background = image.subimage(x, y, soup.width(), soup.height());
-	background.save("background-test.ppm");
-	ppm_image blend = background.alpha_blend(soup, 0.5f);
-	image.replace(blend, x, y);
-	image.save("earth-blend-0.5.ppm");
-}
 }
 
